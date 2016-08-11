3b2b9269b2209ec4e73eda02d2d08e623e4f0490
tomhanson <tomhanson1985@gmail.com>
2016-08-11 17:05:28 +0800

    slider now scrolls infinitely

--- slider.js
+++ slider.js
@@ -48,26 +48,37 @@ Slider.prototype.filter = function() {
 Slider.prototype.setup = function() {
     //array holding all slides
     /* Note: the array is being updated so the variable length is changing everytime I move one from it into the target div */
+    //do some work naming these
     var slideData = this.container.children
+    var slideData1 = [];
+    for(let i=0; i < slideData.length; i++) {
+        slideData1[i] = slideData[i];
+    }
     //get length of slidedata array so I can use it to multiply the tracks width and get a full track
-    var slideLen = slideData.length;
+    var slideLen = slideData1.length;
     //add a class to the container just for lols
     this.container.classList.add('so-container');
     //get viewport width - **THIS IS BEING CALCULATED INCORRECTLY*** this needs go be the width of its container
     var viewportW = this.container.clientWidth;
     this.container.style.width = viewportW + 'px';
+    
+    
+    
+
+    /**** MAKE THIS INTO A PROTOTYPE ****/
     //create a wrapper div to build the slider then give it a class and move all the slide data elements inside it
     var track = document.createElement('div');
     track.classList.add('so-track');
-    //set the length of the track 
-    track.style.width = (slideLen * viewportW) + 'px';
+    track.style.right = 0 + 'px';
+    //set the length of the track add + 2 to accomodate for a cloned slide on each end.
+    track.style.width = ( (slideLen + 2) * viewportW) + 'px';
     //loop through all the slides
-    for(var d=0; d < slideData.length; d++) {
-        slideData[d].classList.add('so-slide');
-        slideData[d].style.width = viewportW + 'px';
+    for(var d=0; d < slideData1.length; d++) {
+        slideData1[d].classList.add('so-slide');
+        slideData1[d].style.width = viewportW + 'px';
     }
-    //set the first slide to active
-    slideData[0].classList.add('active');
+    
+    
     //second loop to get the slides inside their container.
     var slides = document.querySelectorAll('.so-slide')
     //must be a way around this other than 2 for loops!? investigate!
@@ -76,40 +87,76 @@ Slider.prototype.setup = function() {
     }
     
     this.container.appendChild(track);
-    this.movement.call(this, 'slide', viewportW, slides, track);
+        //clone first and last slides
+    this.clone(slideData1, track);
+    this.movement('slide', viewportW, slideData1, track);
+    
+
+    
     //add a resize event here for the slider in full
 }
+/* **** MAKE THIS BE PROTOTYPE.SLIDE AND ADD FADE ETC.. AS SEPERATE OPTIONS **** */
 Slider.prototype.movement = function(type, width, slides, track) {
     //type will effect if its fade/slide etc... needs switch statement when confirmed.
-    var counter = 1;
-    var dynamicWidth = 0;
+    //counter starts at 2 because we are ignoring the first slide and also the second as it has active state on load.
+    var counter = 2;
+    var dynamicWidth = width;
+    track.style.right = dynamicWidth + 'px';
     setInterval(() => {
-
-        if(counter < slides.length) {
-            for(let i=0; i< slides.length; i++) {
+        if(track.style.transition = 'none') {
+         track.style.transition = '2.2s right';   
+        }
+        if(counter <= (slides.length - 2) ) {
+            for(let i=0; i< (slides.length - 1); i++) {
                 slides[i].classList.remove('active');
                 slides[counter].classList.add('active');
             }
             dynamicWidth += width;
             track.style.right = dynamicWidth + 'px';
-        } else {
-            counter = 0;
+        } else if(counter == slides.length - 1) {
+            //start the counter agin and give impression of starting again
+            counter = 1;
             for(let i=0; i< slides.length; i++) {
                 slides[i].classList.remove('active');
                 slides[counter].classList.add('active');
             }
-            dynamicWidth = 0
+            //push the slides along to the final cloned slide
+            dynamicWidth += width;
             track.style.right = dynamicWidth + 'px';
-            
+            setTimeout(function() {
+                track.style.transition = 'none';
+                //note to self - the right always wants to be one slide width less than the full amount
+                dynamicWidth = width
+                track.style.right = dynamicWidth + 'px'; 
+            }, 2200)
         }
-        counter++; 
-    }, 1000);
+        counter++;   
+    }, 3000);
 }
+
+Slider.prototype.clone = function(slides, container){
+    //clone first and last slide
+    var first = slides[0].cloneNode(true);
+    var last = slides[(slides.length - 1)].cloneNode(true);
+    //give the cloned nodes classes;
+    last.classList.add('so-clone-start');
+    first.classList.add('so-clone-end');
+    //add cloned nodes to the ends of the array
+    slides.unshift(last);
+    slides.push(first);
+    //set the first slide to active ignoring the cloned first slide
+    slides[1].classList.add('active');
+    container.insertBefore(last, container.childNodes[0])
+    container.appendChild(first);
+}
+
+
 var slider = document.querySelector('.slider');
 new Slider(slider, {
     slide: false,
     fade: false,
     filter: 'puppyLove',
-    dotType : 'hearts'
+    dotType : 'hearts',
+    slideSpeed: 10000
 });
 
